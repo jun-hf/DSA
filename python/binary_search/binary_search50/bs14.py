0a767e00baf666947d34d095830360a87ecff3ef
jun-hf <fong.junh@gmail.com>
2024-05-06 15:32:29 +0100

    bs 14 and 15

---
+++ python/binary_search/binary_search50/bs14.py
@@ -0,0 +1,23 @@
+import math
+from typing import List
+import binaryTest
+
+def bs(arr: List[int], target: int):
+    min = 0 
+    max = len(arr) -1
+    return recur_binary_search(min, max, arr, target)
+
+def recur_binary_search(min: int, max: int, arr: List[int], target: int):
+    if min > max:
+        return -1 
+    mid = int(min + math.floor((max-min)/2))
+    mid_val = arr[mid]
+
+    if mid_val == target:
+        return mid
+    if mid_val > target:
+        return recur_binary_search(min, mid-1, arr, target)
+    if mid_val < target:
+        return recur_binary_search(mid+1, max, arr, target)
+
+binaryTest.test(bs)
