9fbac7b969ec799e1382aa0b4d5a9b6bdbcb2b4e
jun-hf <fong.junh@gmail.com>
2024-05-06 15:06:41 +0100

    bs 11

---
+++ python/binary_search/binary_search100/bs11.py
@@ -0,0 +1,25 @@
+import math
+from typing import List
+import binaryTest
+
+def bs(arr: List[int], target: int):
+    min = 0
+    max = len(arr)
+    return recur_binary_search(min, max, arr, target)
+
+def recur_binary_search(min: int, max: int, arr: List[int], target: int):
+    # max < min means that return -1
+    # mid_v == target found the mid
+    if max < min:
+        return -1
+    mid = int(min + math.floor((max -min)/2))
+    mid_val = arr[mid]
+
+    if mid_val == target:
+        return mid
+    if mid_val < target:
+        return recur_binary_search(mid+1, max, arr, target)
+    if target < mid_val:
+        return recur_binary_search(min, mid-1, arr, target)
+
+binaryTest.test(bs)
