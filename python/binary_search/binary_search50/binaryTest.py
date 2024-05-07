7f2b384d2b50047090d726df1e2cb633fe6a3258
jun-hf <fong.junh@gmail.com>
2024-05-07 11:08:54 +0100

    binary search 17

--- python/binary_search/binary_search50/binaryTest.py
+++ python/binary_search/binary_search50/binaryTest.py
@@ -2,7 +2,7 @@ def test(fn):
     arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
     if fn(arr, 67) != 18:
-        raise Exception("should be 18")
+        raise Exception("should be 18 got", fn(arr, 67))
 
     if fn(arr, 3) != 1:
         raise Exception("should be 1")
---
+++ python/binary_search/binary_search50/binary_search18.py
@@ -0,0 +1,22 @@
+import math
+from typing import List
+import binaryTest
+
+def binary_search(arr: List[int], target: int):
+    min = 0 
+    max = len(arr) -1
+    return recur_bs(min, max, arr, target)
+
+def recur_bs(min: int, max: int, arr: List[int], target: int):
+    if max < min:
+        return -1
+    mid = int(min + math.floor((max-min)/2))
+    mid_val = arr[mid]
+    if mid_val == target:
+        return mid
+    if mid_val > target:
+        return recur_bs(min, mid-1, arr, target)
+    if mid_val < target:
+        return recur_bs(mid+1, max, arr, target) 
+
+binaryTest.test(binary_search)
